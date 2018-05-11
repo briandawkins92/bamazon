d5fd2b4856701cf547890c6f3b5f08108e306f79
Brian Dawkins <bkdawkins@gmail.com>
2018-05-11 15:05:31 -0700

    mvp complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,5 @@
 /* BEGIN JAVASCRIPT*/
 
-console.log("hello");
-
 var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,12 +8,55 @@ var connection = mysql.createConnection({
     password: "",
     database: "bamazon"
 });
+var inquirer = require("Inquirer");
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("connected");
-});
+function checkStock() {
+    console.log("Greetings! This is what we have in stock");
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " & Product name: "
+             + res[i].product_name);
+            }
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "Item_ID",
+                message: "What is the ID of the product you wish to buy?"
+            }, {
+                type: "input",
+                name: "want_quantity",
+                message: "How much do you wish to buy?"
+            }
+        ]).then(function (product) {
+            var x = product.Item_ID;
+            var y = product.want_quantity;
+            if (y > res[x - 1].stock_quantity) {
+                console.log("Insufficient Quantity!");
+                connection.end();
+            } else {
+                console.log("Your order is fulfilled! Your total is $"
+                    + (y * res[x - 1].PRICE));
+                    var query = connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: (res[x-1].stock_quantity - y)
+                            },
+                            {
+                                item_id: x
+                            }
+                        ]
+                    )
+                    console.log(query.sql);
+                    connection.end();
+                };
+            })
+
+    })
+}
 
 
+checkStock();
 
 /* END JAVASCRIPT*/
+
